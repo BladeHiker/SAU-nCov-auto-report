f2f19d7b9ddb556449f19d9c0f820d4a69c6a5d3
BladeHiker <duanjihang@live.com>
2021-11-26 13:54:06 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -72,7 +72,7 @@ def make_msg(res, daily):
     msg = r'''**智慧沈航打卡结果**
     **{}**
     打卡时间：{}
-    体温：{}-{}-{}
+    体温：{} | {} | {}
     '''.format(res, daily['riqi'], daily['tiwen'], daily['tiwen1'], daily['tiwen2'])
     return msg
 
