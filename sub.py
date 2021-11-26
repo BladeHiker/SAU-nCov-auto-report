488879f949b8671f3cb51c1b79b1e8bfbfd662f8
BladeHiker <duanjihang@live.com>
2021-11-26 13:59:53 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -73,7 +73,7 @@ def make_msg(res, daily):
     **{}**
     打卡时间：{}
     体温：{} | {} | {}
-    '''.format(res, daily['riqi'], daily['tiwen'], daily['tiwen1'], daily['tiwen2'])
+    '''.format(res, daily['riqi'], daily['tiwen'], daily['tiwen1'], daily['tiwen2']).replace("-", "\-")
     return msg
 
 
